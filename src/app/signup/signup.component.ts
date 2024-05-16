85ae40fe4724b339e376af9f60a5beb38db11521
danielhdzf <danielsan13a@gmail.com>
2024-05-16 20:45:32 +0100

    Connected signup danielhdzf/tfm#10

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { SignupService } from '../services/signup.service';
+import { Router } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+
+  @ViewChild('errorAlert') private errorAlert!: ElementRef;
+  @ViewChild('okAlert') private okAlert!: ElementRef;
+  
+  signupform = new FormGroup({
+    username : new FormControl('', [Validators.maxLength(8),Validators.required]),
+    email : new FormControl('', [Validators.required, Validators.email]),
+    password : new FormControl('', [Validators.required])
+  });
+
+  constructor(
+    private signupService: SignupService,
+    private renderer: Renderer2,
+    private router: Router
+  ) {}
+
+  dosignup(data: FormGroup) {
+
+    const { username, password, email } = this.signupform.value;
+
+    this.signupService.registerUser(username!, password!, email!).subscribe(
+      () => {
+        this.renderer.setStyle(this.okAlert.nativeElement, 'display', 'block');
+        setTimeout(() => {
+          
+          this.router.navigate(['/login']);
+        }, 1500);
+      },
+      (error) => {
+        if (error.status === 401) {
+          this.renderer.setStyle(this.errorAlert.nativeElement, 'display', 'block');
+        }
+      }
+    );
+  }
 
 }
