0abae79c44664b06fb6c2fc56ea91e73590adf96
danielhdzf <danielsan13a@gmail.com>
2024-05-17 15:00:35 +0100

    Connected login and login interceptor danielhdzf/tfm#10

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,17 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { LoginInterceptor } from './interceptors/login.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     importProvidersFrom(HttpClientModule),
-    provideRouter(routes)]
+    provideRouter(routes),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoginInterceptor,
+      multi: true
+    }
+  ]
 };
---
+++ src/app/services/login.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  private apiUrl = 'http://localhost:8080/users/login';
+  private loggedIn = new Subject<boolean>();
+
+  constructor(private http: HttpClient) {}
+
+  login(username: string, password: string): Observable<any> {
+    const url = `${this.apiUrl}?username=${username}&password=${password}`;
+    return this.http.get(url, { responseType: 'json', observe: 'response' });
+  }
+
+  getLoggedIn(): Observable<boolean> {
+    return this.loggedIn.asObservable();
+  }
+
+  setLoggedIn(value: boolean) {
+    this.loggedIn.next(value);
+  }
+
+  saveToken(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  clearToken() {
+    localStorage.removeItem('token');
+  }
+
+  getAuthToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+}
