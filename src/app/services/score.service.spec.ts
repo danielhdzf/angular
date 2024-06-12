a335012d63be3b211c27a2ef87e19f5088e6f916
danielhdzf <danielsan13a@gmail.com>
2024-06-12 18:16:46 +0200

    Fixed import error danielhdzf/tfm#20

--- src/app/services/score.service.spec.ts
+++ src/app/services/score.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ScoreService } from './score.service';
 
 describe('ScoreService', () => {
   let service: ScoreService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(ScoreService);
   });
 
